e26643139be11fb84b489ea236a6ec244e0223fb
Emily Coats <emilycoats@riseup.net>
2017-02-05 19:19:47 +0000

    iterated over both fields but it still only stores last value.

--- lcf/forms.py
+++ lcf/forms.py
@@ -11,6 +11,8 @@ class BigForm(forms.Form):
     starting_scenario = Scenario.objects.get(pk=21)
     scenario_name = forms.CharField(label="Scenario name", max_length=200)
     for y in starting_scenario.auctionyear_set.all():
+        z = str(y.year)
+        z = forms.CharField(label=z, max_length=200)
         for t in y.auctionyeartechnology_set.all():
             technology_name = str(t.technology_name)
             technology_name = forms.CharField(label=technology_name, max_length=200)
