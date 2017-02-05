c6c53dfc5947640ecabc85a968f167db8dd5ff3c
Emily Coats <emilycoats@riseup.net>
2017-02-05 19:16:39 +0000

    created a big form. but it doesn't work.

--- lcf/forms.py
+++ lcf/forms.py
@@ -6,3 +6,11 @@ class ScenarioForm(forms.ModelForm):
     class Meta:
         model = Scenario
         fields = ('name','budget', 'percent_emerging',)
+
+class BigForm(forms.Form):
+    starting_scenario = Scenario.objects.get(pk=21)
+    scenario_name = forms.CharField(label="Scenario name", max_length=200)
+    for y in starting_scenario.auctionyear_set.all():
+        for t in y.auctionyeartechnology_set.all():
+            technology_name = str(t.technology_name)
+            technology_name = forms.CharField(label=technology_name, max_length=200)
--- lcf/views.py
+++ lcf/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .forms import ScenarioForm
+from .forms import ScenarioForm, BigForm
 from .models import Scenario
 
 # Create your views here.
 def scenario_new(request):
-    form = ScenarioForm()
     scenarios = Scenario.objects.all()
+    if request.method == "POST":
+        form = BigForm(request.POST)
+        if form.is_valid():
+            scenario = form.save()
+        return redirect('scenario_detail', pk=scenario.pk)
+    else:
+        form = BigForm()
     return render(request, 'lcf/scenario_new.html', {'form': form, 'scenarios': scenarios })
 
-def scenario_save(request):
-    form = ScenarioForm(request.POST)
-    if form.is_valid():
-        scenario = form.save()
-    return redirect('scenario_detail', pk=scenario.pk)
 
 def scenario_detail(request, pk):
     scenario = get_object_or_404(Scenario,pk=pk)
