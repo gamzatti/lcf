91104221e774530aec07e07ee7a39a5e3a20eaf1
Emily Coats <emilycoats@riseup.net>
2017-02-08 17:26:40 +0000

    Start to allocate money between pots and between years.

--- lcf/models.py
+++ lcf/models.py
@@ -10,8 +10,8 @@ class Scenario(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
     date = models.DateTimeField(default=timezone.now)
-    budget = models.DecimalField(default=3.3, max_digits=5, decimal_places=2)
-    percent_emerging = models.DecimalField(default=0.6, max_digits=4, decimal_places=2)
+    budget = models.FloatField(default=3.3)
+    percent_emerging = models.FloatField(default=0.6)
 
     def __str__(self):
         return self.name
@@ -20,7 +20,7 @@ class Scenario(models.Model):
     def results(self):
         auction_tally = {'cost': 0, 'gen': 0}
         for auctionyear in self.auctionyear_set.all():
-            auction_results = auctionyear.run_auction()
+            auction_results = auctionyear.run_auction('E')
             auction_tally['cost'] += auction_results['cost']
             auction_tally['gen'] += auction_results['gen']
         return auction_tally
@@ -32,40 +32,45 @@ class Scenario(models.Model):
         return round(self.results()['gen'],3)
 
 class AuctionYear(models.Model):
-    scenario = models.ForeignKey('lcf.scenario', default=48)#http://stackoverflow.com/questions/937954/how-do-you-specify-a-default-for-a-django-foreignkey-model-or-adminmodel-field
+    scenario = models.ForeignKey('lcf.scenario', default=1)#http://stackoverflow.com/questions/937954/how-do-you-specify-a-default-for-a-django-foreignkey-model-or-adminmodel-field
     year = models.IntegerField(default=2020)
     wholesale_price = models.IntegerField(default=53)
 
     def __str__(self):
         return str(self.year)
 
-    def pot_budget(self):
-        pot_budget = {2020: 196,
-                    2021: 156,
-                    2022: 260,
-                    2023: 327,
-                    2024: 245,
-                    2025: 114,
-                    2026: 66,
-                    2027: 73,
-                    2028: 94,
-                    2029: 272,
-                    2030: 366}
-        return pot_budget[self.year]
+
+    def budget_calc(self, pot):
+        yearly_budget = self.scenario.budget / 5 * 1000
+        if self.year == 2020:
+            yearly_budget = 481.29
+        previous_year_unspent = {2020: 0, 2021: 0, 2022: 349.07, 2023: 524.76 , 2024: 1048.10, 2025: 1616.12, 2026: 2200.54, 2027: 2145.85, 2028: 2093.53, 2029: 2028.08, 2030: 1958.61}
+        yearly_budget += previous_year_unspent[self.year]
+        budget_e = yearly_budget * self.scenario.percent_emerging
+        budget_m = yearly_budget - budget_e
+        if pot == "E":
+            return budget_e
+        if pot == "M":
+            return budget_m
 
 
-    def find_projects(self):
-        return self.auctionyeartechnology_set.filter(pot='E')
 
-    def run_auction(self):
+    def pot_budget_e(self):
+        return self.budget_calc("E")
+
+    def pot_budget_m(self):
+        return self.budget_calc("M")
+
+
+    def run_auction(self,pot):
         cost = 0
         gen = 0
         combined_tech_affordable_projects = Series(name='Combined tech affordable projects')
-        project_lists = self.find_projects()
+        project_lists = self.auctionyeartechnology_set.filter(pot=pot)
         for project_list in project_lists:
             cfd_unit_cost = project_list.strike_price - self.wholesale_price
             for ind in project_list.affordable_projects().index:
-                if cost + cfd_unit_cost * project_list.project_gen_correct() < self.pot_budget():
+                if cost + cfd_unit_cost * project_list.project_gen_correct() < self.budget_calc(pot):
                     combined_tech_affordable_projects[ind] = project_list.affordable_projects()[ind]
                     cost += cfd_unit_cost * project_list.project_gen_correct()
                     gen += project_list.project_gen_correct()
@@ -73,14 +78,14 @@ class AuctionYear(models.Model):
                     break
         return {'cost': cost, 'gen': gen, 'combined_tech_affordable_projects': combined_tech_affordable_projects}
 
-    def cost(self):
-        return round(self.run_auction()['cost'],3)
+    def cost_e(self):
+        return round(self.run_auction('E')['cost'],3)
 
-    def gen(self):
-        return round(self.run_auction()['gen'],3)
+    def gen_e(self):
+        return round(self.run_auction('E')['gen'],3)
 
-    def combined_tech_affordable_projects(self):
-        return self.run_auction()['combined_tech_affordable_projects']
+    def combined_tech_affordable_projects_e(self):
+        return self.run_auction('E')['combined_tech_affordable_projects']
 
 
 class AuctionYearTechnology(models.Model):
