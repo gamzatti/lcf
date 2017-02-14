6c1e4e407eba2ab6bf95380e52253caf159b9619
Emily Coats <emilycoats@riseup.net>
2017-02-14 12:23:53 +0000

    working on subtracting last year's projects from auction

--- lcf/models.py
+++ lcf/models.py
@@ -149,28 +149,38 @@ class Pot(models.Model):
         else:
             return 0
 
+#    @lru_cache(maxsize=None)
     @lru_cache(maxsize=None)
     def run_auction(self):
         gen = 0
         cost = 0
         projects = DataFrame(columns=['levelised_cost', 'gen', 'technology', 'strike_price', 'clearing_price', 'affordable', 'funded'])
+        if self.auctionyear.year == 2020:
+            previous_year_projects = DataFrame()
+        else:
+            previous_year = self.auctionyear.scenario.auctionyear_set.get(year = self.auctionyear.year - 1).pot_set.get(name=self.name)
+            #don't know why the next line doesn't work!!!
+            previous_year_projects = previous_year.projects()[(previous_year.projects().funded == "this year") or (previous_year.projects().funded == "previously")]
         for t in self.technology_set.all():
             t.create_projects()
             aff = t.projects[t.projects.affordable == True]
             unaff = t.projects[t.projects.affordable == False]
             actual_cost = 0
             for i in aff.index:
-                funded_gen = sum(aff.gen[aff.funded==True])
-                attempted_gen = funded_gen + aff.gen[i]
-                attempted_clearing_price = aff.levelised_cost[i]
-                attempted_cost = attempted_gen * (attempted_clearing_price - self.auctionyear.wholesale_price)
-                if attempted_cost < self.budget() - cost:
-                    aff.funded[i] = True
-                    actual_gen = attempted_gen
-                    aff.clearing_price = attempted_clearing_price
-                    actual_cost = attempted_cost
+                if i in previous_year_projects.index:
+                    aff.funded = "previously funded"
                 else:
-                    break
+                    funded_gen = sum(aff.gen[aff.funded=="this year"])
+                    attempted_gen = funded_gen + aff.gen[i]
+                    attempted_clearing_price = aff.levelised_cost[i]
+                    attempted_cost = attempted_gen * (attempted_clearing_price - self.auctionyear.wholesale_price)
+                    if attempted_cost < self.budget() - cost:
+                        aff.funded[i] = "this year"
+                        actual_gen = attempted_gen
+                        aff.clearing_price = attempted_clearing_price
+                        actual_cost = attempted_cost
+                    else:
+                        break
             projects = pd.concat([projects,aff,unaff])
             cost += actual_cost
             gen += actual_gen
@@ -193,10 +203,11 @@ class Pot(models.Model):
 
     @lru_cache(maxsize=None)
     def funded_projects(self):
-        df = self.run_auction()['projects']
-        return df[df.funded == True]
+        return projects()[projects().funded == "this year"]
 
-
+    @lru_cache(maxsize=None)
+    def projects(self):
+        return self.run_auction()['projects']
 
 
 
@@ -228,6 +239,7 @@ class Technology(models.Model):
         super(Technology, self).__init__(*args, **kwargs)
         self.projects = None
         self.project_gen = self.project_gen_incorrect / 1000
+        self.cum_project_gen = self.cum_project_gen_incorrect / 1000
         self.project_cap = cap(self.project_gen,self.load_factor)
         self.years_supported = 2031 - self.pot.auctionyear.year
         self.max_deployment_gen = gen(self.max_deployment_cap,self.load_factor)
@@ -240,14 +252,14 @@ class Technology(models.Model):
     @lru_cache(maxsize=None)
     def create_projects(self):
         dep = Series(np.linspace(self.min_levelised_cost,self.max_levelised_cost,self.num_projects+2)[1:-1],name="levelised_cost")
-        gen = Series(np.repeat(self.project_gen,len(dep)),name='gen')
+        gen = Series(np.repeat(self.cum_project_gen,len(dep)),name='gen')
         projects = pd.concat([dep,gen], axis=1)
         projects.index = [ self.name + str(i + 1) for i in range(len(dep)) ]
         projects['technology'] = self.name
         projects['strike_price'] = self.strike_price
         projects['clearing_price'] = np.nan
         projects['affordable'] = projects.levelised_cost <= projects.strike_price
-        projects['funded'] = np.nan
+        projects['funded'] = 'no'
         self.projects = projects
         return None
 
--- scribble.py
+++ scribble.py
@@ -10,7 +10,45 @@ t = p.technology_set.get(name="OFW")
 
 
 
+new:
+    @lru_cache(maxsize=None)
+    def run_auction(self):
+        gen = 0
+        cost = 0
+        projects = DataFrame(columns=['levelised_cost', 'gen', 'technology', 'strike_price', 'clearing_price', 'affordable', 'funded'])
+        if self.auctionyear.year == 2020:
+            previous_year_projects = DataFrame()
+        else:
+            previous_year = self.auctionyear.scenario.auctionyear_set.get(year = self.auctionyear.year - 1).pot_set.get(name=self.name)
+            previous_year_projects = previous_year.projects()[(previous_year.projects().funded == "this year") or (previous_year.projects().funded == "previously")]
+        for t in self.technology_set.all():
+            t.create_projects()
+            aff = t.projects[t.projects.affordable == True]
+            unaff = t.projects[t.projects.affordable == False]
+            actual_cost = 0
+            for i in aff.index:
+                if i in previous_year_projects.index:
+                    aff.funded = "previously funded"
+                else:
+                    funded_gen = sum(aff.gen[aff.funded=="this year"])
+                    attempted_gen = funded_gen + aff.gen[i]
+                    attempted_clearing_price = aff.levelised_cost[i]
+                    attempted_cost = attempted_gen * (attempted_clearing_price - self.auctionyear.wholesale_price)
+                    if attempted_cost < self.budget() - cost:
+                        aff.funded[i] = "this year"
+                        actual_gen = attempted_gen
+                        aff.clearing_price = attempted_clearing_price
+                        actual_cost = attempted_cost
+                    else:
+                        break
+            projects = pd.concat([projects,aff,unaff])
+            cost += actual_cost
+            gen += actual_gen
 
+        return {'cost': cost, 'gen': gen, 'projects': projects}
+
+old:
+class Pot:
     def run_auction(self):
         gen = 0
         cost = 0
@@ -21,12 +59,12 @@ t = p.technology_set.get(name="OFW")
             unaff = t.projects[t.projects.affordable == False]
             actual_cost = 0
             for i in aff.index:
-                funded_gen = sum(aff.gen[aff.funded==True])
+                funded_gen = sum(aff.gen[aff.funded=="this year"])
                 attempted_gen = funded_gen + aff.gen[i]
                 attempted_clearing_price = aff.levelised_cost[i]
                 attempted_cost = attempted_gen * (attempted_clearing_price - self.auctionyear.wholesale_price)
                 if attempted_cost < self.budget() - cost:
-                    aff.funded[i] = True
+                    aff.funded[i] = "this year"
                     actual_gen = attempted_gen
                     aff.clearing_price = attempted_clearing_price
                     actual_cost = attempted_cost
